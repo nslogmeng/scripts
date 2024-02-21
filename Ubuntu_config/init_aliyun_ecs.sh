4bde39e8131b465e091b7191e33d6fb48bb95260
Meng <meng94233@gmail.com>
2024-02-21 18:03:04 +0800

    feat: add aliyun setup shell

---
+++ Ubuntu_config/init_aliyun_ecs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "start initial Aliyun ECS..."
+
+echo "current shell: $SHELL"
+echo "support shells:"
+cat /etc/shells
+
+echo "install required packages..."
+sudo apt-get update
+sudo apt-get  -y --no-install-recommends git curl zsh autojump
+
+# clone scripts repos
+TMPDIR=$(mktemp -d) || exit 1
+trap 'rm -rf "$TMPDIR"' EXIT
+git clone https://github.com/nslogmeng/Scripts.git TMPDIR
+
+# change to zsh
+chsh -s /bin/zsh
+
+echo "install oh-my-zsh..."
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install oh-my-zsh custom plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+mv $TMPDIR/Ubuntu_config/aliyun_ecs_zshrc ~/.zshrc
+
+source ~/.zshrc
