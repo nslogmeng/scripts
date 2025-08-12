0514e81f0652a19eef07944754e40b9c786f3a92
Meng <nslogmeng@gmail.com>
2025-08-13 00:39:17 +0800

    chore: update macOS init.sh

--- macOS/init.sh
+++ macOS/init.sh
@@ -3,14 +3,25 @@
 GREEN_COLOR='\033[0;32m'
 RESET_COLOR='\033[0m'
 
+set -e
+
 echo "${GREEN_COLOR}start initial macOS..${RESET_COLOR}"
 
-echo "${GREEN_COLOR}install home brew...${RESET_COLOR}"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# check homebrew
+if ! command -v brew &> /dev/null; then
+    echo "${GREEN_COLOR}install home brew...${RESET_COLOR}"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "${GREEN_COLOR}homebrew already installed.${RESET_COLOR}"
+fi
 
-echo "${GREEN_COLOR}install oh-my-zsh...${RESET_COLOR}"
-# install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# check oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "${GREEN_COLOR}install oh-my-zsh...${RESET_COLOR}"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "${GREEN_COLOR}oh-my-zsh already installed.${RESET_COLOR}"
+fi
 
 # clone scripts repos
 echo "${GREEN_COLOR}fetch script repo...${RESET_COLOR}"
@@ -25,11 +36,28 @@ source ~/.zshrc
 
 echo "${GREEN_COLOR}install oh-my-zsh plugins...${RESET_COLOR}"
 # install oh-my-zsh custom plugins
-echo "${GREEN_COLOR}brew install autojump...${RESET_COLOR}"
-brew install autojump
-echo "${GREEN_COLOR}install zsh-autosuggestions...${RESET_COLOR}"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-echo "${GREEN_COLOR}install zsh-syntax-highlighting...${RESET_COLOR}"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+# check autojump
+if ! command -v autojump &> /dev/null; then
+    echo "${GREEN_COLOR}install autojump...${RESET_COLOR}"
+    brew install autojump
+else
+    echo "${GREEN_COLOR}autojump already installed.${RESET_COLOR}"
+fi
+
+# check zsh-autosuggestions
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    echo "${GREEN_COLOR}install zsh-autosuggestions...${RESET_COLOR}"
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+else
+    echo "${GREEN_COLOR}zsh-autosuggestions already installed.${RESET_COLOR}"
+fi
+
+# check zsh-syntax-highlighting
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    echo "${GREEN_COLOR}install zsh-syntax-highlighting...${RESET_COLOR}"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+else
+    echo "${GREEN_COLOR}zsh-syntax-highlighting already installed.${RESET_COLOR}"
+fi
 
 echo "${GREEN_COLOR}initial macOS done!${RESET_COLOR}"
